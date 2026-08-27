9488f2afb977ab2ea3d65f25efe6b6c971ed4ee8
Subhajit das <311803584+Subhajit083@users.noreply.github.com>
2026-08-27 10:54:15 +0530

    ✅ Best Divisor | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-27

---
+++ hackerrank/medium/best-divisor/solution.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+        int bestSum =1;
+        int best =1;
+        for(int i=1; i<=n;i++){
+            if(n%i==0){
+                int sum=0;
+                int x=i;
+                while(x!=0){
+                    sum+=x%10;
+                    x/=10;
+                }
+                if(sum>bestSum){
+                    bestSum=sum;
+                    best=i;
+                }
+            }
+        }
+        System.out.println(best);
+
+        bufferedReader.close();
+    }
+}
