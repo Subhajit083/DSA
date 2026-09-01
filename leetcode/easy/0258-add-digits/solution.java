80062aa13eca315e613ad2212bbbbc261fb3aef9
Subhajit das <311803584+Subhajit083@users.noreply.github.com>
2026-09-01 10:40:47 +0530

    ✅ Add Digits | Easy | Java | Runtime: 1 ms (98.11%) | Memory: 42.4 MB | 2026-09-01

---
+++ leetcode/easy/0258-add-digits/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumOfDigit(int n){
+        int sum =0;
+        while(n!=0){
+            sum+=n%10;
+            n/=10;
+        }
+        return sum;
+    }
+    public int addDigits(int num) {
+        while(num>=10){
+            num=sumOfDigit(num);
+        }
+        return num;
+    }
+}
