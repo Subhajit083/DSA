506449b340712183e6884ecc39f5afb0327281ef
Subhajit das <311803584+Subhajit083@users.noreply.github.com>
2026-09-02 20:38:29 +0530

    ✅ MISSINGNUM7 | Medium | Java | Runtime: N/A | Memory: N/A | 2026-09-02

---
+++ codechef/medium/MISSINGNUM7/solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc= new Scanner(System.in);
+		
+		int S = sc.nextInt();
+		int missingNumber = 10-S;
+		System.out.println(missingNumber);
+		
+		sc.close();
+
+	}
+
+}
